ddd3907d7b3e80e5583dbf4123791b16cf3bafc5
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-09-06 10:58:17 +0500

    fegrdhtfyg

--- backend/controllers/ProductsController.php
+++ backend/controllers/ProductsController.php
@@ -26,7 +26,7 @@ class ProductsController extends Controller
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-
+                        'delete' => ['POST'],
                     ],
                 ],
             ]
--- backend/controllers/SlideController.php
+++ backend/controllers/SlideController.php
@@ -24,7 +24,7 @@ class SlideController extends Controller
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
-
+                        'delete' => ['POST'],
                     ],
                 ],
             ]
