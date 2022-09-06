529eaa15567a6e137dde2828b2985bc20bac1266
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-09-06 16:46:16 +0500

    fdbftnyu

--- backend/views/site/message.php
+++ backend/views/site/message.php
@@ -1,5 +1,5 @@
 
-<div class="card" style="width: 18rem;">
+<div class="card" style="width: 60rem;">
     <div class="card-body">
         <h3 class="card-title"><?=$xabar->message;?></h3>
     </div>
