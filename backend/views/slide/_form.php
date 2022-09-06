97cd09d814036ec4b67feb77ca0efe1030392036
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-09-06 11:37:07 +0500

    Slide upload file

--- backend/views/slide/_form.php
+++ backend/views/slide/_form.php
@@ -14,7 +14,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'imageFile')->fileInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
