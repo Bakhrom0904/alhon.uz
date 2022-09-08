d44f00ffacb261d687c2637d43d5382ad9f4a76c
Bakhrom0904 <bahromislomov0904@gmail.com>
2022-09-09 03:38:58 +0500

    working api contact

--- api/controllers/ContactController.php
+++ api/controllers/ContactController.php
@@ -12,4 +12,34 @@ class ContactController  extends ActiveController
 {
     public $modelClass = 'common\models\Contact';
 
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+//                // restrict access to
+                'Origin' => [
+                    '*'
+                ],
+            ],
+        ];
+
+
+
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions=parent::actions();
+        unset($actions['index']);
+        unset($actions['view']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        return $actions;
+    }
+
 }
